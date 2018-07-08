225504694fe1f1d57155f198d42d9ddeca278b53
cute1baby <1138724604@qq.com>
2018-07-08 10:52:48 +0800

    update1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import 'babel-polyfill' // 解决es6的API补丁问题
-import  ig.productionTip = false
-Vue.use(VueLazyload)
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store'
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-unused-vars */
+// import vConsole from 'vconsole'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
